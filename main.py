676d61240cef9d69d8d3bc9053b2a220923a0bfa
Nicholas Sellati <nsellati@gmail.com>
2022-07-21 19:12:18 -0700

    Initial settings

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import sys
 import random
 from math import *
 
+pygame.init()
+
+width = 700
+height = 600
+
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Balloon_Pop")
+clock = pygame.time.Clock()
+
+margin = 100
+lowerBound = 100
+score = 0
